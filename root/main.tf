a1e4bf9ce5275ce6c1d253bf3b09556debcee022
Akinyemi Akinkunle <akinyemiakinkunle14@gmail.com>
2024-08-28 22:20:52 +0000

    Updated main.tf

--- root/main.tf
+++ root/main.tf
@@ -72,7 +72,7 @@ output "client_id_value" {
  */
 
 data "azurerm_shared_image_version" "kun" {
-  name                = "1.0.4"
+  name                = "1.0.5"
   image_name          = "acgImageDef2"
   gallery_name        = "acgAk"
   resource_group_name = "kun"
